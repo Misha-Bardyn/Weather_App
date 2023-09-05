d6ebeb7ca9cbff386f1df7350cd62cf13a2f3f32
Misha-Bardyn <bardynmisha@gmail.com>
2023-09-05 17:18:03 +0300

    Updates

---
+++ precache-manifest.cbfa1cb28713061b2a8c9924330d53af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b6af0b287e5a148a3d0",
+    "url": "/Weather_App/static/css/main.63c2685b.chunk.css"
+  },
+  {
+    "revision": "7b6af0b287e5a148a3d0",
+    "url": "/Weather_App/static/js/main.7b6af0b2.chunk.js"
+  },
+  {
+    "revision": "f7afca65d233c50480b0",
+    "url": "/Weather_App/static/js/1.f7afca65.chunk.js"
+  },
+  {
+    "revision": "37277faa78cb53a7da57",
+    "url": "/Weather_App/static/js/runtime~main.37277faa.js"
+  },
+  {
+    "revision": "251036b979c46fb6da5cb0787ae8223c",
+    "url": "/Weather_App/index.html"
+  }
+];
